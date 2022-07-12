e218bcfa39f668411293bbeedfe80871c8ca8be0
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-13 03:06:49 +0800

    Add updateCart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,6 +8,7 @@ import '../models/product.dart';
 
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+  static const headers = {'Content-type': 'application/json'};
 
   Future<APIResponse> getToken(String username, String password) {
     return http.post(
@@ -116,6 +117,51 @@ class ApiService {
         final personCart = Cart.fromJson(jsonData);
 
         print('Product ID $id has been successfully deleted');
+      }
+    });
+  }
+
+  Future<void> updateCart(int cartId, int productId) async {
+    APIResponse<Cart> oldCart = await getCart(cartId);
+    APIResponse<Product> newProduct = await getSingleProduct(productId);
+
+    print('Before adding...');
+    Cart randomCart = oldCart.data!;
+    print(json.encode(randomCart));
+
+    bool isExisting = false;
+
+    Cart newCart = oldCart.data!;
+    for (var eachItem in newCart.products!) {
+      if (eachItem['productId'] == productId + 1) {
+        int thisIndex = newCart.products!.indexOf(eachItem);
+        newCart.products![thisIndex]['quantity'] =
+            newCart.products![thisIndex]['quantity'] + 1;
+        isExisting = true;
+        break;
+      }
+    }
+    if (isExisting == false) {
+      newCart.products!.add({'productId': newProduct.data!.id, 'quantity': 1});
+    }
+
+    newCart.date = DateTime.now();
+
+    return http
+        .patch(
+      Uri.parse('$baseUrl/carts/$cartId '),
+      body: json.encode(newCart.toJson()),
+      headers: headers,
+    )
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        print('After adding...');
+        print(jsonData);
+
+        print(
+          'Product ID ${productId + 1} has been successfully added to the cart',
+        );
       }
     });
   }
