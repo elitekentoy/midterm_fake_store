41e936a7c6edb0ccbcc8df8b3d4ca36ed58dc80c
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-12 20:09:01 +0800

    Add fromJson

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -4,7 +4,16 @@ class Cart {
   int? id;
   int? userId;
   DateTime? date;
-  List<Product>? products;
+  List<dynamic>? products;
 
   Cart({this.id, this.userId, this.date, this.products});
+
+  factory Cart.fromJson(Map<String, dynamic> data) {
+    return Cart(
+      id: data['id'],
+      userId: data['userId:'],
+      date: DateTime.parse(data['date']),
+      products: data['products'],
+    );
+  }
 }
