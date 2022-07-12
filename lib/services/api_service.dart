aa307e383dbdd4d6fc96fc2e2f3f0b99e26e83fc
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-12 13:44:10 +0800

    Add alias to http package

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,5 @@
 import 'package:fake_store/models/api_response.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
