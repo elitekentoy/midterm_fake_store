b2409cc9470a8baf8e3fce8937318bba682c4525
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-12 14:46:19 +0800

    Add getToken for login

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,18 @@
+import 'dart:convert';
+
 import 'package:fake_store/models/api_response.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<APIResponse> getToken(String username, String password) {
+    return http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      body: {"username": username, "password": password},
+    ).then((data) {
+      final jsonData = json.decode(data.body);
+      return APIResponse(data: jsonData);
+    });
+  }
 }
