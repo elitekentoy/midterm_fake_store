6036b150512c2a442f8a1d3b0a9a9b0e15c03f80
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-12 17:58:16 +0800

    Import APIResponse and Implement Service

--- lib/screens/products_by_category.dart
+++ lib/screens/products_by_category.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
+import '../models/api_response.dart';
 import '../models/product.dart';
 import '../services/api_service.dart';
 import 'product_detail.dart';
@@ -21,8 +22,9 @@ class ProductsByCategoryScreen extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: FutureBuilder(
-        future: getProductsByCategory(categoryName),
-        builder: (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
+        future: service.getProductsByCategory(categoryName),
+        builder: (BuildContext context,
+            AsyncSnapshot<APIResponse<List<Product>>> snapshot) {
           if (!snapshot.hasData) {
             return const Center(child: CircularProgressIndicator());
           }
