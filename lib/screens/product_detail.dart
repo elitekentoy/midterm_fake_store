d866a3b8e62a48c7b2fef53b4207f5ca7c898fa8
Jonathan Kent Morales <jonathankentmorales@gmail.com>
2022-07-12 16:44:17 +0800

    Comment out floating action button

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -84,18 +84,18 @@ class ProductDetailScreen extends StatelessWidget {
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.green,
-        onPressed: () async {
-          await updateCart(1, id);
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Product added to cart'),
-            ),
-          );
-        },
-        child: const Icon(Icons.add_shopping_cart),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.green,
+      //   onPressed: () async {
+      //     await updateCart(1, id);
+      //     ScaffoldMessenger.of(context).showSnackBar(
+      //       const SnackBar(
+      //         content: Text('Product added to cart'),
+      //       ),
+      //     );
+      //   },
+      //   child: const Icon(Icons.add_shopping_cart),
+      // ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
